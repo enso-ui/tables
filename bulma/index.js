9f4a5069f0174d26e18f7ad53a292fe6b6260e26
mishu <mishu@xtelecom.ro>
2019-09-20 15:59:05 +0300

    removes filterState

--- bulma/index.js
+++ bulma/index.js
@@ -1,5 +1,4 @@
 import VueTable from '../src/bulma/VueTable.vue';
 import EnsoTable from '../src/bulma/EnsoTable.vue';
-import FilterState from '../src/renderless/FilterState.vue';
 
-export { VueTable, EnsoTable, FilterState };
+export { VueTable, EnsoTable };
