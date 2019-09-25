e30f04aa7fb469df33d9cadbdfff8d2f60e6662b
mishu <mishu@xtelecom.ro>
2019-09-25 14:44:15 +0300

    small cleanup in vResponsive

--- src/bulma/responsive/vResponsive.js
+++ src/bulma/responsive/vResponsive.js
@@ -2,18 +2,17 @@ import { debounce } from 'lodash';
 import { ResizeSensor } from 'css-element-queries';
 import ResponsiveTable from './ResponsiveTable';
 
-let table = null;
-
 export default {
     inserted: (el, binding, { context }) => {
         if (binding.value) {
-            table = new ResponsiveTable(el, context);
+            const table = new ResponsiveTable(el, context);
             context._resizeSensor = new ResizeSensor(el, debounce(() => table && table.fit(), 16));
         }
     },
 
     unbind: (el, binding, { context }) => {
-        table = null;
-        context._resizeSensor = null;
+        if (binding.value) {
+            context._resizeSensor = null;
+        }
     },
 };
