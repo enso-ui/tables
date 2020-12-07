dc479062fbdf29049f2c1e3bcba6dfcfb6fd6d05
mishu <mishu@xtelecom.ro>
2020-12-07 10:21:57 +0200

    adds a small padding between controls

--- src/bulma/parts/top/Controls.vue
+++ src/bulma/parts/top/Controls.vue
@@ -5,68 +5,70 @@
                 forceInfoEvents, i18n, reloadEvents, resetEvents, state,
             }">
             <div class="wrapper">
-            <div class="top-controls has-background-light">
-                <div class="columns is-multiline is-mobile is-gapless">
-                    <div class="column table-controls is-narrow-desktop is-half-touch">
-                        <length-menu v-if="state.template.controls.includes('length')"/>
-                        <column-visibility v-if="state.template.controls.includes('columns')"/>
-                        <style-selector class="is-hidden-mobile"
-                            v-if="state.template.controls.includes('style')"/>
-                        <a class="button"
-                            v-on="reloadEvents"
-                            v-if="state.template.controls.includes('reload')">
-                            <span class="icon is-small">
-                                <fa icon="sync"/>
-                            </span>
-                        </a>
-                        <a class="button"
-                            v-on="resetEvents"
-                            v-if="state.template.controls.includes('reset')">
-                            <span class="icon is-small">
-                                <fa icon="undo"/>
-                            </span>
-                        </a>
-                        <a class="button"
-                            v-on="forceInfoEvents"
-                            v-if="!state.body.fullRecordInfo">
-                            <span class="icon is-small has-text-info">
-                                <fa icon="info-circle"/>
-                            </span>
-                        </a>
-                    </div>
-                    <div class="column table-buttons is-narrow-desktop is-half-touch has-text-right"
-                        v-if="state.template.buttons">
-                        <template v-for="button in state.template.buttons.global">
-                            <slot :name="button.slot"
-                                :icon="button.icon"
-                                :label="button.label"
-                                :css-class="button.class"
-                                v-if="button.slot"/>
-                            <a class="button has-margin-left-small"
-                                :class="button.class"
-                                :key="`${button.label}-${button.icon}`"
-                                v-bind="controlBindings(button)"
-                                v-on="controlEvents(button)"
-                                v-else>
-                               <span class="is-hidden-mobile">
-                                   {{ i18n(button.label) }}
-                               </span>
-                                   <span class="icon is-small">
-                                   <fa :icon="button.icon"/>
-                               </span>
-                               <span class="is-hidden-mobile"/>
+                <div class="top-controls has-background-light">
+                    <div class="columns is-multiline is-mobile is-variable is-1">
+                        <div class="column table-controls is-narrow-desktop is-half-touch">
+                            <length-menu class="mr-1"
+                                v-if="state.template.controls.includes('length')"/>
+                            <column-visibility class="mr-1"
+                                v-if="state.template.controls.includes('columns')"/>
+                            <style-selector class="is-hidden-mobile mr-1"
+                                v-if="state.template.controls.includes('style')"/>
+                            <a class="button mr-1"
+                                v-on="reloadEvents"
+                                v-if="state.template.controls.includes('reload')">
+                                <span class="icon is-small">
+                                    <fa icon="sync"/>
+                                </span>
                             </a>
-                        </template>
-                    </div>
-                    <div class="column search">
-                        <search ref="search"/>
+                            <a class="button"
+                                v-on="resetEvents"
+                                v-if="state.template.controls.includes('reset')">
+                                <span class="icon is-small">
+                                    <fa icon="undo"/>
+                                </span>
+                            </a>
+                            <a class="button"
+                                v-on="forceInfoEvents"
+                                v-if="!state.body.fullRecordInfo">
+                                <span class="icon is-small has-text-info">
+                                    <fa icon="info-circle"/>
+                                </span>
+                            </a>
+                        </div>
+                        <div class="column table-buttons is-narrow-desktop is-half-touch has-text-right"
+                            v-if="state.template.buttons">
+                            <template v-for="button in state.template.buttons.global">
+                                <slot :name="button.slot"
+                                    :icon="button.icon"
+                                    :label="button.label"
+                                    :css-class="button.class"
+                                    v-if="button.slot"/>
+                                <a class="button has-margin-left-small"
+                                    :class="button.class"
+                                    :key="`${button.label}-${button.icon}`"
+                                    v-bind="controlBindings(button)"
+                                    v-on="controlEvents(button)"
+                                    v-else>
+                                <span class="is-hidden-mobile">
+                                    {{ i18n(button.label) }}
+                                </span>
+                                    <span class="icon is-small">
+                                    <fa :icon="button.icon"/>
+                                </span>
+                                <span class="is-hidden-mobile"/>
+                                </a>
+                            </template>
+                        </div>
+                        <div class="column search">
+                            <search ref="search"/>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <scenarios class="has-padding-left-medium has-padding-right-medium"
-                v-if="filterScenarios"/>
-            <labels class="has-padding-left-medium has-padding-right-medium"
-                v-if="filterLabels"/>
+                <scenarios class="has-padding-left-medium has-padding-right-medium"
+                    v-if="filterScenarios"/>
+                <labels class="has-padding-left-medium has-padding-right-medium"
+                    v-if="filterLabels"/>
             </div>
         </template>
     </core-controls>
