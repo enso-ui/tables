30be91083f504f6da6dd7e39413af368cbb9a39c
mishu <mishu@xtelecom.ro>
2021-03-05 14:41:18 +0200

    fixes boolean classes on slot

--- src/bulma/parts/TableCell.vue
+++ src/bulma/parts/TableCell.vue
@@ -1,19 +1,19 @@
 <template>
-    <span :class="[boolean, clickable, icon]"
-        @click="column.meta.clickable ? $emit('clicked') : null">
-        <slot v-if="column.meta.slot"
+    <span :class="cssClass"
+        @click="meta.clickable ? $emit('clicked') : null">
+        <slot v-if="meta.slot"
             :name="column.name">
             {{ value }}
         </slot>
         <fa :icon="value ? 'check' : 'times'"
             size="sm"
-            v-else-if="column.meta.boolean"/>
+            v-else-if="meta.boolean"/>
         <fa :icon="value"
-            v-else-if="column.meta.icon && value"/>
+            v-else-if="meta.icon && value"/>
         <template v-else-if="column.enum">
             {{ column.enum._get(value) }}
         </template>
-        <template v-else-if="column.meta.translatable">
+        <template v-else-if="meta.translatable">
             {{ i18n(value) }}
         </template>
         <template v-else>{{ value }}</template>
@@ -42,20 +42,27 @@ export default {
     },
 
     computed: {
+        cssClass() {
+            return [this.boolean, this.clickable, this.icon]
+                .filter(v => v).join(' ');
+        },
         boolean() {
-            return this.column.meta.boolean
+            return this.meta.boolean && !this.meta.slot
                 ? ['tag is-table-tag icon', this.value ? 'is-success' : 'is-danger']
                 : null;
         },
         clickable() {
-            return this.column.meta.clickable
+            return this.meta.clickable
                 ? 'is-clickable has-text-info'
                 : null;
         },
         icon() {
-            return this.column.meta.icon
+            return this.meta.icon
                 ? 'icon is-small'
                 : null;
+        },
+        meta() {
+            return this.column.meta;
         },
     },
 };
