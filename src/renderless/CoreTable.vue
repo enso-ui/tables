083ab3678c91904bea5fe50a65a9c22c438adfbf
Vlad Chiovianu <vlad.chiovianu@earthlink.ro>
2021-06-16 22:26:57 +0300

    fixes page toggling

--- src/renderless/CoreTable.vue
+++ src/renderless/CoreTable.vue
@@ -457,8 +457,10 @@ export default {
             if (!this.isChild(row)) {
                 const index = this.state.selected
                     .findIndex(id => id === row[this.template.dtRowId]);
-                if (!this.state.pageSelected) {
-                    this.state.selected.splice(index, 1);
+                if(! this.state.pageSelected) {
+                    if(index !== -1) {
+                        this.state.selected.splice(index, 1);
+                    }
                 } else if (index === -1) {
                     this.state.selected.push(row[this.template.dtRowId]);
                 }
